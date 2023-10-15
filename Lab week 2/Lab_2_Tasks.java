ffb4a6f73916485c5a07856b6a5ae91418f72153
Idris-2 <idris.hasanovic@stu.ibu.edu.ba>
2023-10-15 16:54:44 +0200

    Finished homework.

--- Lab week 2/Lab_2_Tasks.java
+++ Lab week 2/Lab_2_Tasks.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 class Lab_2_Tasks{
     public static void main(String[] args){
@@ -66,7 +67,7 @@ class Lab_2_Tasks{
                 System.out.println(i);
             }
         }
-*/
+
 
         // 5. Create a program that calculates the sum of 20+21+22+...+2n
         System.out.println("Enter number of squence sum: ");
@@ -77,6 +78,98 @@ class Lab_2_Tasks{
         }
         System.out.println("The result is: " + String.valueOf(sum_seq));
         
+
+        // 6. Create a method printText that prints the following string of characters: "In the beginning there were the swamp, the hoe and Java." and a line break.
+        printText();
+
+
+        // 7. Create a method printStars that prints the given amount of stars and a line break.
+        printStars(4);
+        printStars(6);
+
+
+        // 8. Create a method called drawStarsPiramid that will accept the number of rows as a parameter and print a half piramid shape where the number of rows will be dictated by the sent parameter. 
+        drawStarsPiramid(5);
+
+
+        // 9. Modify the previously created starts piramid in a way that now you are not printing the starts but in each row numbers from 1 to the current row count.
+        drawNumPiramid(5);
+*/
+        // 10. In this exercise, the following game is created:
+        beginGame();
+
         idk.close();
+    }
+    public static void printText(){     // 6.
+        Scanner lmao = new Scanner(System.in);
+        System.out.println("Enter number of repetitions: ");
+        int lol = lmao.nextInt();
+        for(int i = 0; i < lol; i++){
+        System.out.println("In the beginning there were the swamp, the hoe and Java." + "\n");
+        }
+        lmao.close();
+    }
+
+    public static void printStars(int x){       // 7.
+        for(int i = 0; i < x; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void drawStarsPiramid(int row){       // 8.
+        for(int i = 0; i <= row; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        // STAR FLIP:
+        System.out.println("\n");
+        for(int i = row; i > 0; i--){
+            for(int j = i; j > 0; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void drawNumPiramid(int row){     // 9.
+        for(int i = 0; i <= row; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
+        // OTHER REQUESTD OUTPUT:
+        for(int i = 0; i <= row; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void beginGame(){
+        Scanner user = new Scanner(System.in);
+        Random rand = new Random();
+        int rn = rand.nextInt(101);
+        // System.out.println(rn);
+        int count = 0;
+
+        while (true){
+            System.out.print("Input number: ");
+            int user_in = user.nextInt();
+            count++;
+            
+            if(user_in == rn){
+                System.out.println("Congrats! You guessed the number in " + Integer.toString(count) + " tries!");
+                break;
+            }
+            else if(user_in < rn){System.out.println("The number is greater then that!" + "\n" + "Guesses made: " + Integer.toString(count) + "\n");}
+            else{System.out.println("Number is lower then that!" + "\n" + "Guesses made: " + Integer.toString(count) + "\n");}
+        }
+        user.close();
     }
 }
