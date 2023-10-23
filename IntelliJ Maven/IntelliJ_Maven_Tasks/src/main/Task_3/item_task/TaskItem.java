f5b260602c7b44000843f107badf94edbd32a868
Idris-2 <idris.hasanovic@stu.ibu.edu.ba>
2023-10-23 13:06:49 +0200

    I have no idea wtf im doing lol

---
+++ IntelliJ Maven/IntelliJ_Maven_Tasks/src/main/Task_3/item_task/TaskItem.java
@@ -0,0 +1,22 @@
+package Lab_week_3.item_task;
+
+import Lab_week_3.enums.enums;
+
+public class TaskItem {
+    private int taskId;
+    private String taskDescription;
+    private enums status;
+
+    public TaskItem(int taskId, String taskDescription, enums lol){
+        this.taskId = taskId;
+        this.taskDescription = taskDescription;
+        this.status = lol;
+    }
+
+    public int getTaskId(){return this.taskId;}
+    public void setTaksId(int taskId){this.taskId = taskId;}
+    public String getTaskDesc(){return this.taskDescription;}
+    public void setTaskDesc(String TaskDesc){this.taskDescription = TaskDesc;}
+    public enums GetStatus(){return this.status;}
+    public void setStatus(enums status){this.status = status;}
+}
